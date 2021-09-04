966b327c019f4133b3fc157276aff00b22e154a9
Vasilii Redinskii <redinskiy@yandex.ru>
2021-09-04 06:01:30 +0300

    Vers3

--- Pizza.py
+++ Pizza.py
@@ -8,7 +8,10 @@ class Item:
     
     @staticmethod
     def check_title(title):
-        return title is not ''
+        if title:
+            return True
+        else:
+            return False
 
     @property
     def title(self):
@@ -21,13 +24,70 @@ class Item:
         else:
             raise ValueError   
 
+class Number:
+    def __init__(self, number):
+        
+        if Number.check_number(number):
+            self.__number = number
+        else:
+            raise ValueError
+    
+    @staticmethod
+    def check_number(number):
+        return number > 0
 
-instance = Item('gbr')
+    @property
+    def number(self):
+        return self.__number
+    
+    @number.setter
+    def number(self, number):
+        if Number.check_number(number):
+            self.__number = number
+        else:
+            raise ValueError 
+class Product(Item):
 
-# На этой строке получаем ошибку, потому что сработала проверка в сеттере
-instance.title = input()
+    def __init__(self, title, calorific, cost):
+        super().__init__(title)
+        if Number(calorific) and Number(cost):
+            self.calorific = calorific
+            self.cost = cost
+        else:
+            raise ValueError
+              
+        
+    def get_calorific(self, weight):
+        # Обращаемся к атрибутам экземпляра класса через self.
+        return weight * self.calorific / 100    
+    
+    def get_cost(self, weight):
+        # Обращаемся к атрибутам экземпляра класса через self.
+        return weight * self.cost / 100  
 
+class Ingredient():
+    def __init__(self, product, weight):
+        self.product = product
+        if Number(weight):
+            self.weight = weight
+        else:
+            raise ValueError
 
+    def get_calorific(self):
+        return self.product.get_calorific(self.weight)
+
+    def get_cost_ingredient(self):
+        return self.product.get_cost(self.weight)
+
+class Pizza(Item):
+    def __init__(self, title, ingredients):
+        super().__init__(title)
+        self.ingredients=ingredients
+
+    def __str__(self):
+        return self.title
+
+        
 # Создаем продукты с указанием названия, калорийности продукта и его себестоимости
 dough_product = Product('Тесто', 200, 20)
 tomato_product = Product('Помидор', 100, 50)
@@ -35,10 +95,10 @@ cheese_product = Product('Сыр', 100, 120)
 
 # Из продуктов создаем ингредиенты. Для каждого ингредиента указываем продукт, 
 # из которого он состоит и вес продукта
-dough_ingredient = Ingredient(dough_product, 100)
-tomato_ingredient = Ingredient(tomato_product, 100)
+dough_ingredient = Ingredient(dough_product, 200)
+tomato_ingredient = Ingredient(tomato_product, -100)
 cheese_ingredient = Ingredient(cheese_product, 100)
-
+print(tomato_ingredient.get_calorific())
 # Из ингредиентов создаем пиццу
 pizza_margarita = Pizza('Маргарита', [dough_ingredient, tomato_ingredient, cheese_ingredient])
 
